1b04c53e15bdaa2aef6880067076191f00bc6da4
Katarzyna Bożek <bozekka2183_aehit@students.vizja.pl>
2026-05-04 19:43:20 +0200

    Lab_3 done

---
+++ Dotnet/MvcMovie/Controllers/HelloWorldController.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MvcMovie.Models;
+
+namespace MvcMovie.Controllers
+{
+    public class HelloWorldController : Controller
+    {
+        public IActionResult Welcome(string name, int numTimes = 1)
+        {
+            var model = new HelloWorldModel
+            {
+                Name = name,
+                NumTimes = numTimes
+            };
+
+            return View(model);
+        }
+    }
+}
