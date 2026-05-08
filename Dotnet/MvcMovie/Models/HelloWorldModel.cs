e393b508a7707e57c280bd4fd481b04047f85610
Katarzyna Bożek <bozekka2183_aehit@students.vizja.pl>
2026-05-08 22:02:31 +0200

    done lab 3 v 2

--- Dotnet/MvcMovie/Models/HelloWorldModel.cs
+++ Dotnet/MvcMovie/Models/HelloWorldModel.cs
@@ -2,7 +2,7 @@
 {
     public class HelloWorldModel
     {
-        public string Name { get; set; }
+        public string? Name { get; set; }
         public int NumTimes { get; set; }
     }
 }
--- Dotnet/MvcMovie/Program.cs
+++ Dotnet/MvcMovie/Program.cs
@@ -1,7 +1,9 @@
 using Microsoft.EntityFrameworkCore;
 using MvcMovie.Data;
+using MvcMovie.Models;
 
 var builder = WebApplication.CreateBuilder(args);
+
 
 
 
@@ -11,6 +13,11 @@ builder.Services.AddDbContext<MvcMovieContext>(options =>
     options.UseSqlServer(builder.Configuration.GetConnectionString("MvcMovieContext")));
 
 var app = builder.Build();
+using (var scope = app.Services.CreateScope())
+{
+    var services = scope.ServiceProvider;
+    SeedData.Initialize(services);
+}
 
 
 if (!app.Environment.IsDevelopment())
